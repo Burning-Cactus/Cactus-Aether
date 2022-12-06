183087ee5a140da52a5eeb1cb3013a475fc7bb85
Archie <azwerlein77@gmail.com>
2022-12-05 21:11:30 -0600

    Initialize the repository

---
+++ src/main/java/com/burningcactus/cactus_aether/CactusAether.java
@@ -0,0 +1,8 @@
+package com.burningcactus.cactus_aether;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(CactusAether.MODID)
+public class CactusAether {
+    public static final String MODID = "cactus_aether";
+}
